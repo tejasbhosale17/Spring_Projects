951a8289ed21abc274dbbf1b46b8548cf68dbeba
Tejas Bhosale <50192323+tejasbhosale17@users.noreply.github.com>
2023-06-05 12:33:20 +0530

    changes in customer created individual functions

--- menudriven_curd_customer/src/main/java/customerMain/customerCURD.java
+++ menudriven_curd_customer/src/main/java/customerMain/customerCURD.java
@@ -10,16 +10,115 @@ import cunstomerConnection.DBUtil;
 
 
 public class customerCURD {
-	private static Connection connection;
+	private static Connection con;
+	static Scanner scan = new Scanner(System.in); 
 	static {
 		try {
-			connection = DBUtil.getConnection();
+			con = DBUtil.getConnection();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+//--------------------------------------------------------------------
+	public static void showAllCustomer(Connection con) {
+		System.out.println("List of All Products");
+		try {
+			String sql="select * from customer";
+			PreparedStatement ptmt = con.prepareStatement(sql);
 
+			try(ResultSet rs= ptmt.executeQuery()){
+				while(rs.next()) {
+					System.out.println(rs.getInt("id")+" "+rs.getString("name")+" "+rs.getLong("mobile")+" "+rs.getString("email")+" "+rs.getString("address"));
+				}
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		System.out.println("");
+	}
+
+//-----------------------------------------------------------------------
+
+	public static void specificCustomer(Connection con) {
+		System.out.println("Specific Customer");
+		String q2 ="select * from customer where id= ? ;";
+		try {
+			PreparedStatement ptmt = con.prepareStatement(q2);
+			System.out.println("enter id of the customer:");
+			int num=scan.nextInt();
+			ptmt.setInt(1, num);
+			try{
+				ResultSet rs =ptmt.executeQuery();
+					while(rs.next()) {
+						System.out.println(num+" "+rs.getString("name")+" "+rs.getLong("mobile")+" "+rs.getString("email")+" "+rs.getString("address"));
+					}			
+				}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println("");
+	}
+//---------------------------------------------------------------------
+	public static void removeCustomer(Connection con) {
+		System.out.println("Deleted Customer!");
+		String q3 ="delete from customer where id= ?";
+		try {
+			PreparedStatement pmt= con.prepareStatement(q3);
+			int deln =scan.nextInt();
+			pmt.setInt(1, deln);
+			int row =pmt.executeUpdate();
+			System.out.println("rows affected "+row);
+			System.out.println("Delected Succesfully!");
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println("");
+	}
+//--------------------------------------------------------------------
+	public static void changeCustomer(Connection con) {
+		try {
+			PreparedStatement updateProduct = con.prepareStatement("UPDATE products SET price = ? WHERE id = ?");
+			System.out.print("Enter the product id to update = ");
+			int id = scan.nextInt();
+			System.out.print("Enter the product price = ");
+			double price = scan.nextDouble();
+			updateProduct.setDouble(1, price);
+			updateProduct.setInt(2, id);
+			updateProduct.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("");
+	}
+//---------------------------------------------------------------------
+	public static void addCustomer(Connection con) {
+		System.out.println("Added Customer!");
+		String q5="insert into customer values( ? , ? , ? , ? , ? )";
+		try {
+			PreparedStatement pmt =con.prepareStatement(q5);
+			System.out.println("Enter id, name, mno,wmail,addr");
+			pmt.setInt(1,scan.nextInt() );
+			pmt.setString(2, scan.next());
+			pmt.setLong(3, scan.nextLong());
+			pmt.setString(4, scan.next());
+			pmt.setString(5, scan.next());
+			int insnum =pmt.executeUpdate();
+			System.out.println("rows affected "+insnum);
+			System.out.println("Insertion Succesful!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("");
+	}
+	
 	public static int menu() {
 		System.out.println("0.Exit");
 		System.out.println("1.Fetch All Products");
@@ -33,104 +132,29 @@ public class customerCURD {
 	}
 	
 public static void main(String[] args) {
+	customerCURD cust = new customerCURD();
 	int choice;
 	Scanner scan = new Scanner(System.in);
 	while((choice = menu())!=0) {
 		switch (choice) {
 		case 1:
-				System.out.println("List of All Products");
-				try {
-					String sql="select * from customer";
-					PreparedStatement ptmt = connection.prepareStatement(sql);
-	
-					try(ResultSet rs= ptmt.executeQuery()){
-						while(rs.next()) {
-							System.out.println(rs.getInt("id")+" "+rs.getString("name")+" "+rs.getLong("mobile")+" "+rs.getString("email")+" "+rs.getString("address"));
-						}
-					}catch(Exception e) {
-						e.printStackTrace();
-					}
-				} catch (SQLException e1) {
-					e1.printStackTrace();
-				}
-				System.out.println("");
+				cust.showAllCustomer(con);
 			break;
 
 		case 2:
-			System.out.println("Specific Customer");
-			String q2 ="select * from customer where id= ? ;";
-			try {
-				PreparedStatement ptmt = connection.prepareStatement(q2);
-				System.out.println("enter id of the customer:");
-				int num=scan.nextInt();
-				ptmt.setInt(1, num);
-				try{
-					ResultSet rs =ptmt.executeQuery();
-						while(rs.next()) {
-							System.out.println(num+" "+rs.getString("name")+" "+rs.getLong("mobile")+" "+rs.getString("email")+" "+rs.getString("address"));
-						}			
-					}
-				catch(Exception e) {
-					e.printStackTrace();
-				}
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			System.out.println("");
+				cust.specificCustomer(con);
 			break;
 
 		case 3:
-			System.out.println("Deleted Customer!");
-			String q3 ="delete from customer where id= ?";
-			try {
-				PreparedStatement pmt= connection.prepareStatement(q3);
-				int deln =scan.nextInt();
-				pmt.setInt(1, deln);
-				int row =pmt.executeUpdate();
-				System.out.println("rows affected "+row);
-				System.out.println("Delected Succesfully!");
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			System.out.println("");
+				cust.removeCustomer(con);
 			break;
 
 		case 4:
-				try {
-					PreparedStatement updateProduct = connection.prepareStatement("UPDATE products SET price = ? WHERE id = ?");
-					System.out.print("Enter the product id to update = ");
-					int id = scan.nextInt();
-					System.out.print("Enter the product price = ");
-					double price = scan.nextDouble();
-					updateProduct.setDouble(1, price);
-					updateProduct.setInt(2, id);
-					updateProduct.execute();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				System.out.println("");
+				cust.changeCustomer(con);
 			break;
 
 		case 5:
-			System.out.println("Added Customer!");
-			String q5="insert into customer values( ? , ? , ? , ? , ? )";
-			try {
-				PreparedStatement pmt =connection.prepareStatement(q5);
-				System.out.println("Enter id, name, mno,wmail,addr");
-				pmt.setInt(1,scan.nextInt() );
-				pmt.setString(2, scan.next());
-				pmt.setLong(3, scan.nextLong());
-				pmt.setString(4, scan.next());
-				pmt.setString(5, scan.next());
-				int insnum =pmt.executeUpdate();
-				System.out.println("rows affected "+insnum);
-				System.out.println("Insertion Succesful!");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			System.out.println("");
+				cust.addCustomer(con);
 			break;
 
 			
